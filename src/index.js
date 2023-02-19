915750682bc30997d7758cf70856281d8b452603
gammahazard <92896466+gammahazard@users.noreply.github.com>
2023-02-19 01:11:47 -0500

    adding src

--- src/index.js
+++ src/index.js
@@ -40,7 +40,14 @@ async function connect() {
   audioNFTsContainer.style.display = "block";
   let audioNFTsHTML = '';
 
-  
+  audioNFTs.forEach((audioNFT) => {
+    audioNFTsHTML += `
+      <div>
+        <p class = "assettitle">CyberVerse  Audio NFT:</p><p class="assetdescription"> ${audioNFT.name}</p>
+      </div>
+    `;
+  });
+  audioNFTsContainer.innerHTML = audioNFTsHTML;
 }
     async function displayCypxAmount(userAddress) {
     const response = await fetch(`https://api.ergoplatform.com/api/v1/boxes/unspent/byAddress/${userAddress}`);
@@ -57,6 +64,11 @@ async function connect() {
     return cypxAmount;
     
     
+    }
+    function formatTime(seconds) {
+      const minutes = Math.floor(seconds / 60);
+      const remainingSeconds = Math.floor(seconds % 60);
+      return `${minutes}:${remainingSeconds.toString().padStart(2, '0')}`;
     }
     async function displayCybercitizenAssets(userAddress) {
       const response = await fetch(`https://api.ergoplatform.com/api/v1/boxes/unspent/byAddress/${userAddress}`);
@@ -107,7 +119,7 @@ async function connect() {
     
       data.items.forEach((item) => {
         item.assets.forEach((asset) => {
-          if (['Laser Guns', 'Outrun', 'Danger Zone', 'Cyberlykos', 'Blue Lights', 'Into Cyberia', 'Cyborgstein'].includes(asset.name)) {
+          if (['Laser Guns', 'Outrun', 'Danger Zone', 'Cyberlykos', 'Blue Lights', 'Into Cyberia'].includes(asset.name)) {
             audioNFTs.push({
               name: asset.name,
             });
@@ -118,39 +130,122 @@ async function connect() {
       let audioNFTsHTML = '';
       audioNFTs.forEach((audioNFT) => {
         audioNFTsHTML += `
-        <div class="assetcont">
-        <p class="assettitle">CyberVerse Audio Track:</p>
-        <p class="assetdescription">${audioNFT.name}</p>
-        <button class="play-button" data-asset="${audioNFT.name}">Play</button>
-        <button class="pause-button" data-asset="${audioNFT.name}">Pause</button>
-      </div>
+          <div class="assetcont">
+            <p class="assettitle">CyberVerse Track Name:</p>
+            <p class="assetdescription">${audioNFT.name}</p>
+            <button class="play-button" data-asset="${audioNFT.name}">Play</button>
+            <button class="pause-button" data-asset="${audioNFT.name}" style="display:none;">Pause</button>
+            <div class="seek-container">
+              <input class="seek-bar" type="range" min="0" step="1" value="0">
+              <span class="current-time">0:00 </span><span>/</span>
+              <span class="duration">0:00</span>
+            </div>
+          </div>
         `;
       });
     
       const audioNFTsElement = document.getElementById("audio-nfts");
-audioNFTsElement.innerHTML = audioNFTsHTML;
-
-// add event listener to play buttons
-const playButtons = document.querySelectorAll('.play-button');
-const pauseButtons = document.querySelectorAll('.pause-button');
-playButtons.forEach(button => {
-  const assetName = button.dataset.asset;
-  button.addEventListener('click', () => playAudio(assetName));
-});
-pauseButtons.forEach(button => {
-  const assetName = button.dataset.asset;
-  button.addEventListener('click', () => audio.pause(assetName));
-});
-
-function playAudio(assetName) {
-
-  const formattedAssetName = assetName.replace(/ /g, '_');
-  const audio = new Audio(`./dist/assets/audio-dashboard/${formattedAssetName}.wav`);
-  audio.play();
-
-
+      audioNFTsElement.innerHTML = audioNFTsHTML;
     
-  
-      }}
-connectbtn.addEventListener("click", connect)
+      // add event listeners to play buttons
+      const playButtons = document.querySelectorAll('.play-button');
+      playButtons.forEach(button => {
+        const assetName = button.dataset.asset;
+        const audio = new Audio(`./dist/assets/audio-dashboard/${assetName.replace(/ /g, '_')}.wav`);
+        let audioPlayer;
+    
+        const seekBar = button.parentElement.querySelector('.seek-bar');
+        const currentTime = button.parentElement.querySelector('.current-time');
+        const duration = button.parentElement.querySelector('.duration');
+    
+        audio.addEventListener('loadedmetadata', () => {
+          duration.textContent = formatTime(audio.duration);
+          seekBar.max = audio.duration;
+        });
+    
+        seekBar.addEventListener('input', () => {
+          currentTime.textContent = formatTime(seekBar.value);
+          audio.currentTime = seekBar.value;
+        });
+    
+        button.addEventListener('click', () => {
+          audio.play();
+          audioPlayer = audio;
+          button.style.display = 'none';
+          button.nextElementSibling.style.display = 'inline-block'; // show the pause button
+        });
+    
+        // add event listeners to pause buttons
+        const pauseButtons = document.querySelectorAll('.pause-button');
+        pauseButtons.forEach(pauseButton => {
+          pauseButton.addEventListener('click', () => {
+            audioPlayer.pause();
+            pauseButton.style.display = 'none';
+            pauseButton.previousElementSibling.style.display = 'inline-block'; // show the play button
+          });
+        });
+    
+        // update seek bar as audio plays
+        audio.addEventListener('timeupdate', () => {
+          seekBar.value = audio.currentTime;
+          currentTime.textContent = formatTime(audio.currentTime);
+        });
+    
+        // add event listener to window to check if
+    
+    
+    
 
+    window.addEventListener('blur', () => {
+    audioNFTs.forEach(audioNFT => {
+    const audio = new Audio(`./dist/assets/audio-dashboard/${audioNFT.name.replace(/ /g, '_')}.wav`);
+    let isPlaying = false;
+    let duration = 0;
+    let seekBar = null;
+    
+   
+      audio.addEventListener('play', () => {
+        isPlaying = true;
+        duration = audio.duration;
+        seekBar.max = duration;
+      });
+    
+      audio.addEventListener('pause', () => {
+        isPlaying = false;
+      });
+    
+      audio.addEventListener('timeupdate', () => {
+        if (isPlaying) {
+          seekBar.value = audio.currentTime;
+        }
+      });
+    
+      const playButton = document.querySelector(`.play-button[data-asset="${audioNFT.name}"]`);
+      const pauseButton = document.querySelector(`.pause-button[data-asset="${audioNFT.name}"]`);
+      seekBar = document.querySelector(`.seek-bar[data-asset="${audioNFT.name}"]`);
+    
+      playButton.addEventListener('click', () => {
+        audio.play();
+        isPlaying = true;
+        playButton.style.display = 'none';
+        pauseButton.style.display = 'inline-block';
+      });
+    
+      pauseButton.addEventListener('click', () => {
+        audio.pause();
+        isPlaying = false;
+        pauseButton.style.display = 'none';
+        playButton.style.display = 'inline-block';
+      });
+    
+      seekBar.addEventListener('input', () => {
+        audio.currentTime = seekBar.value;
+      });
+    });
+    });
+    }
+    
+
+    )}
+
+    connectbtn.addEventListener("click", connect);
